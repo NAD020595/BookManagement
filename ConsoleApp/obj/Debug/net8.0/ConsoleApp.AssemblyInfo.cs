3184f66d9307e488e5d8e19918cf71ac606490e1
Duy Nguyen <duy.nguyen1@trginternational.com>
2025-07-04 16:16:04 +0700

    Build BookSingleView class

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee15a8d12100e84fc19f08ce003a667c7c0674a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83465c0495d42ae4e93790e3d4cfcd54cf2a3f10")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
